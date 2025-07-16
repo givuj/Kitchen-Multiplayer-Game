e45ffba4ff24dcbd8a1cf8322433aa75694dbbf7
givuj <2920152382@qq.com>
2025-07-16 17:09:22 +0800

    交换系统

--- KitchenChaos/Assets/Script/Player.cs
+++ KitchenChaos/Assets/Script/Player.cs
@@ -6,11 +6,57 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask counterslayerMask;
+
     private bool isWalking;
+    private Vector3 lastInteractDir;
+
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
     private void Update()
     {
+        HandleMovement();
+        HandleInteraction();
+    }
+
+    //碰撞检测后获得碰撞物体的对象
+    private void HandleInteraction()
+    {
         Vector2 inputVector = gameInput.GameMovementVectorNormalized();
-       
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);//方向
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+
+        }
+        float interactDistance = 2f;
+
+        Debug.DrawRay(transform.position, lastInteractDir * interactDistance, Color.red, 1f);
+
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, counterslayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        }
+        
+
+    }
+
+    //碰撞检查
+    private void HandleMovement()
+    {
+        Vector2 inputVector = gameInput.GameMovementVectorNormalized();
+
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
         //人物的碰撞检查
@@ -18,22 +64,23 @@ public class Player : MonoBehaviour
         float playerRadius = .7f;
         float playerHeight = 2f;
         bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
-        if(!canMove)
+        if (!canMove)
         {
             Vector3 moveDirx = new Vector3(moveDir.x, 0, 0).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirx, moveDistance);
-            if(canMove)
+            if (canMove)
             {
                 moveDir = moveDirx;
-            }else
+            }
+            else
             {
                 Vector3 moveDirz = new Vector3(0, 0, moveDir.z);
                 canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirz, moveDistance);
-                if(canMove)
+                if (canMove)
                 {
                     moveDir = moveDirz;
                 }
-                
+
             }
         }
         if (canMove)
@@ -43,7 +90,7 @@ public class Player : MonoBehaviour
         isWalking = (moveDir != Vector3.zero);
 
         float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward,moveDir,Time.deltaTime*rotateSpeed);
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
     public bool IsWalking()
     {
