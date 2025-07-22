062ba0616babe09ec3e9dfcc17d3ebe3f284ef43
givuj <2920152382@qq.com>
2025-07-22 14:48:12 +0800

    切菜动画

--- KitchenChaos/Assets/Script/CuttingCounter.cs
+++ KitchenChaos/Assets/Script/CuttingCounter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,20 @@ using UnityEngine;
 public class CuttingCounter : BaseCounter
 {
     // Start is called before the first frame update
+    public event EventHandler<OnProgressChangedEventArgs> OnProgressChanged;
+    public class OnProgressChangedEventArgs : EventArgs
+    {
+        public float progressNormalized;
+    }
+    public event EventHandler OnCut;
+
     [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
+    public int cuttingProgress=0;//切菜的进程
+
+    public void Update()
+    {
+        Debug.Log(cuttingProgress);
+    }
     public override void Interact(Player player)
     {
         //如果柜台没有物品就能放东西
@@ -13,7 +27,17 @@ public class CuttingCounter : BaseCounter
         {
             if (player.HasKitchenObject())//人物手中有食物
             {
-                player.GetKitchenObject().SetKitchenObjectParant(this);
+                
+                if (HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))//看能不能切，有些食物是不能切的，就不能放在柜台上
+                {
+                    cuttingProgress = 0;
+                    player.GetKitchenObject().SetKitchenObjectParant(this);
+                  
+                    CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
+                    OnProgressChanged?.Invoke(this, new OnProgressChangedEventArgs 
+                                                    {progressNormalized=(float)cuttingProgress/cuttingRecipeSO.cuttingProgressMax });
+
+                }
             }
         }
         //如果柜台有物品
@@ -33,24 +57,45 @@ public class CuttingCounter : BaseCounter
 
     public override void InteractF(Player player)
     {
-        if(HasKitchenObject())
+        if(HasKitchenObject()&&HasRecipeWithInput(GetKitchenObject().GetKitchenObjectSO()))// 切一次不能切第二次
         {
+            cuttingProgress++;
+            OnCut?.Invoke(this,EventArgs.Empty);
+            CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
             KitchenObjectSO outputkitchenObjectSO = GetOutputForInput(GetKitchenObject().GetKitchenObjectSO());
-            GetKitchenObject().DestorySelf();
-            
-            KitchenObject.SpawnKitchenObject(outputkitchenObjectSO, this);
-            
+         
+            OnProgressChanged?.Invoke(this, new OnProgressChangedEventArgs
+            { progressNormalized = (float)cuttingProgress / cuttingRecipeSO.cuttingProgressMax });
+            if (cuttingProgress >= cuttingRecipeSO.cuttingProgressMax)
+            {
+                GetKitchenObject().DestorySelf();
+
+                KitchenObject.SpawnKitchenObject(outputkitchenObjectSO, this);
+            }
         }
     }
-
+    private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeSOWithInput(inputKitchenObjectSO);
+        return cuttingRecipeSO != null;
+    }
     //将食物剁碎
     private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO)
     {
-        foreach(CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray)
+        CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeSOWithInput(inputKitchenObjectSO);
+        if(cuttingRecipeSO!=null)
         {
-            if(cuttingRecipeSO.input == inputKitchenObjectSO)
+            return cuttingRecipeSO.output;
+        }
+        return null;
+    }
+    private CuttingRecipeSO GetCuttingRecipeSOWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        foreach (CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray)
+        {
+            if (cuttingRecipeSO.input == inputKitchenObjectSO)
             {
-                return cuttingRecipeSO.output;
+                return cuttingRecipeSO;
             }
         }
         return null;
---
+++ KitchenChaos/Assets/Script/CuttingCounterVisual.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuttingCounterVisual : MonoBehaviour
+{
+    [SerializeField] private CuttingCounter cuttingCounter;
+    private Animator animator;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Start()
+    {
+        cuttingCounter.OnCut += CuttingCounter_OnCut;
+    }
+
+    private void CuttingCounter_OnCut(object sender, System.EventArgs e)
+    {
+        animator.SetTrigger("Cut");
+    }
+
+    // Update is called once per frame
+
+}
